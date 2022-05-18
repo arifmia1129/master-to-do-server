8d200ddf0b7d17e13bef3a218b0be4fa83d2a9bb
arifvtti <arif.vtti@gmail.com>
2022-05-19 04:35:21 +0600

    make an api that receive data from client side

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuaft.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db("MasterToDo").collection("task");
+
+        app.post("/task", async (req, res) => {
+            const doc = req.body;
+            const result = await taskCollection.insertOne(doc);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from master to do server.")
+})
+
+app.listen(port, () => {
+    console.log(`Master To Do listening on port ${port}`);
+})
