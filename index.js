fb5cd005ca406544dc7000fb866221d013c37cbb
arifvtti <arif.vtti@gmail.com>
2022-05-19 06:25:14 +0600

    added an api that delete task

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ async function run() {
             const result = await taskCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         })
+        app.delete("/task/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(filter);
+            res.send(result);
+        })
     }
     finally {
 
